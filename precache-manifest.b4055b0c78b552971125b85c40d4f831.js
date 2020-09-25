7401b4b5f125fa558f84e8ae42b6abea9ea461d9
thureintun234 <thurein12456@gmail.com>
2020-09-25 15:31:14 +0630

    Updates

---
+++ precache-manifest.b4055b0c78b552971125b85c40d4f831.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "233d62b899b74415586fcbd7d9815cb7",
+    "url": "/React-todolist-app/index.html"
+  },
+  {
+    "revision": "3bb947717b3917560dae",
+    "url": "/React-todolist-app/static/css/main.bb677a26.chunk.css"
+  },
+  {
+    "revision": "5db7f2d4b03ed35435c7",
+    "url": "/React-todolist-app/static/js/2.03b3a3f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todolist-app/static/js/2.03b3a3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bb947717b3917560dae",
+    "url": "/React-todolist-app/static/js/main.6ba50b12.chunk.js"
+  },
+  {
+    "revision": "e451451f59554d23f3fa",
+    "url": "/React-todolist-app/static/js/runtime-main.72d51d5d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-todolist-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
